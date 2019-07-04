53821a9493881f950ac30d51642a68ee927ea2dc
Egor <egorka1391@gmail.com>
2019-07-04 23:14:09 +0500

    v 0.0.2

---
+++ precache-manifest.3b9bfe51da098eff1f48a7182ce5a65c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "80481e24b3c6b4b9130fb6b703e69ffe",
+    "url": "/static/media/remove_icon.80481e24.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d61c1cc6d9d711ea4e4",
+    "url": "/static/js/main.e367432d.chunk.js"
+  },
+  {
+    "revision": "e113a98669e64d9f3476",
+    "url": "/static/js/2.16f65239.chunk.js"
+  },
+  {
+    "revision": "9d61c1cc6d9d711ea4e4",
+    "url": "/static/css/main.55c8a6ba.chunk.css"
+  },
+  {
+    "revision": "828e0ab020a12bae5a1bd407e235fded",
+    "url": "/index.html"
+  }
+];
